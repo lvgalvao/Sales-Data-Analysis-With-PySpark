f7e496c741a3ff6cd739eacfc2f64bb6288dfdf4
Luciano Filho <lvgalvaofilho@gmail.com>
2023-07-07 07:58:29 -0300

    feat: add save to csv file in pipeline

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,73 +1,60 @@
 import os
-
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, count, sum
+from pyspark.sql import functions as F
 
-# Inicia uma sessão Spark
-spark = SparkSession.builder.appName('salesAnalysis').getOrCreate()
+# Create Spark session
+spark = SparkSession.builder.master('local').appName('salesAnalysis').getOrCreate()
 
-# Define o caminho para a pasta de dados
-data_dir = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), '..', 'data'
-)
+# Define the path to the output directory
+output_dir = 'data/results'
 
-# Carrega todos os arquivos CSV
-sales_df = spark.read.csv(
-    os.path.join(data_dir, 'sales_data.csv'), header=True, inferSchema=True
-)
-products_df = spark.read.csv(
-    os.path.join(data_dir, 'products_data.csv'), header=True, inferSchema=True
-)
-stores_df = spark.read.csv(
-    os.path.join(data_dir, 'stores_data.csv'), header=True, inferSchema=True
-)
-salespeople_df = spark.read.csv(
-    os.path.join(data_dir, 'salespeople_data.csv'),
-    header=True,
-    inferSchema=True,
-)
-customers_df = spark.read.csv(
-    os.path.join(data_dir, 'customers_data.csv'), header=True, inferSchema=True
-)
+# Read the data from CSV files
+sales_df = spark.read.csv('data/sales_data.csv', header=True, inferSchema=True)
+products_df = spark.read.csv('data/products_data.csv', header=True, inferSchema=True)
+stores_df = spark.read.csv('data/stores_data.csv', header=True, inferSchema=True)
+salespeople_df = spark.read.csv('data/salespeople_data.csv', header=True, inferSchema=True)
+customers_df = spark.read.csv('data/customers_data.csv', header=True, inferSchema=True)
 
-
-# Junta todos os dados em um único DataFrame
-df = (
-    sales_df.join(
-        products_df, sales_df.fk_product_id == products_df.product_id
-    )
-    .join(stores_df, sales_df.fk_store_id == stores_df.store_id)
-    .join(
-        salespeople_df,
-        sales_df.fk_salesperson_id == salespeople_df.salesperson_id,
-    )
+# Join the dataframes
+analysis_datafame = sales_df.join(products_df, sales_df.fk_product_id == products_df.product_id) \
+    .join(stores_df, sales_df.fk_store_id == stores_df.store_id) \
+    .join(salespeople_df, sales_df.fk_salesperson_id == salespeople_df.salesperson_id) \
     .join(customers_df, sales_df.fk_customer_id == customers_df.customer_id)
-)
 
 # Análise de desempenho do vendedor
-salespeople_performance = df.groupBy('salesperson_name').agg(
-    sum('sales_amount').alias('total_sales'),
-    count('customer_id').alias('num_customers'),
-)
-salespeople_performance.show()
+salespeople_performance = analysis_datafame.groupBy('salesperson_name') \
+    .agg(F.format_number(F.sum('sales_amount'), 0).alias('total_sales'),
+         F.approx_count_distinct('customer_id').alias('num_customers')) \
+    .orderBy(F.col('total_sales').desc())
+
+# Save salespeople_performance as CSV
+salespeople_performance.write.csv(os.path.join(output_dir, 'salespeople_performance'), header=True, mode='overwrite')
+
 
 # Análise de produtos
-product_analysis = df.groupBy('product_name').agg(
-    sum('sales_amount').alias('total_sales'),
-    count('customer_id').alias('num_customers'),
-)
-product_analysis.show()
+product_performance = analysis_datafame.groupBy('product_name') \
+    .agg(F.format_number(F.sum('sales_amount'), 0).alias('total_sales'),
+         F.approx_count_distinct('customer_id').alias('num_customers')) \
+    .orderBy(F.col('total_sales').desc())
+
+# Save product_analysis as CSV
+product_performance.write.csv(os.path.join(output_dir, 'product_performance'), header=True, mode='overwrite')
 
 # Análise de lojas
-store_analysis = df.groupBy('store_name').agg(
-    sum('sales_amount').alias('total_sales'),
-    count('customer_id').alias('num_customers'),
-)
-store_analysis.show()
+store_analysis = analysis_datafame.groupBy('store_name') \
+    .agg(F.format_number(F.sum('sales_amount'), 0).alias('total_sales'),
+         F.approx_count_distinct('customer_id').alias('num_customers')) \
+    .orderBy(F.col('total_sales').desc())
+
+# Save store_analysis as CSV
+store_analysis.write.csv(os.path.join(output_dir, 'store_analysis'), header=True, mode='overwrite')
 
 # Análise de clientes
-customer_analysis = df.groupBy('customer_name').agg(
-    sum('sales_amount').alias('total_sales'),
-    count('product_id').alias('num_products'),
-)
-customer_analysis.show()
+customer_analysis = analysis_datafame.groupBy('customer_name') \
+    .agg(F.format_number(F.sum('sales_amount'), 0).alias('total_purchase'),
+         F.approx_count_distinct('product_id').alias('num_products')) \
+    .orderBy(F.col('total_purchase').desc())
+
+# Save customer_analysis as CSV
+customer_analysis.write.csv(os.path.join(output_dir, 'customer_analysis'), header=True, mode='overwrite')
+
